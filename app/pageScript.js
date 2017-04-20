55b0bf3122d2eeee77f22d250c71407bdcdcfc95
Sanat Shah <sunnyshah711@gmail.com>
2017-04-20 00:47:35 -0400

    progress on nlp

--- app/pageScript.js
+++ app/pageScript.js
@@ -1,18 +1,46 @@
 
   //grab all text from document
   var allText = {};
-  allText.p = document.getElementsByTagName("p");
-  allText.a = document.getElementsByTagName("a");
-  allText.button = document.getElementsByTagName("button");
-  allText.li = document.getElementsByTagName("li");
-  allText.h1 = document.getElementsByTagName("h1");
-  allText.h2 = document.getElementsByTagName("h2");
-  allText.h3 = document.getElementsByTagName("h3");
-  allText.h4 = document.getElementsByTagName("h4");
+
+  allText.p = [];
+  var p = document.getElementsByTagName("p");
+  for (var i=0; i<p.length; i++){
+    allText.p.push(p[i].innerHTML);
+  }
+
+  allText.a = [];
+  var a = document.getElementsByTagName("a");
+  for (var i=0; i<a.length; i++){
+    allText.a.push(a[i].innerHTML);
+  }
+
+  allText.button = [];
+  var button = document.getElementsByTagName("button");
+  for (var i=0; i<button.length; i++){
+    allText.button.push(button[i].innerHTML);
+  }
+
+  allText.li = [];
+  var li = document.getElementsByTagName("li");
+  for (var i=0; i<li.length; i++){
+    allText.li.push(li[i].innerHTML);
+  }
+
+  allText.h1 = [];
+  var h1 = document.getElementsByTagName("h1");
+  for (var i=0; i<h1.length; i++){
+    allText.h1.push(h1[i].innerHTML);
+  }
+
+  allText.h2 = [];
+  var h2 = document.getElementsByTagName("h2");
+  for (var i=0; i<h2.length; i++){
+    allText.h2.push(h2[i].innerHTML);
+  }
 
   //send to nlp
   chrome.runtime.sendMessage({request: "getTopicProbability", value: allText},
     function (response){
-      alert("got response" + response);
+      console.log(response.value);
     }
   );
--- processor/nlp.js
+++ processor/nlp.js
@@ -1,11 +1,40 @@
+  const NOUN = "noun";
+  const VERB = "verb";
+  const ADJECTIVE = "adjective";
+
   chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
 
     if (response.request == "getTopicProbability"){
     //returns probability that this page pertains to the topic
+      var allText = response.value;
 
-      var num = nlp('five-hundred and twenty');
-      num.values().toNumber();
-      sendResponse({value: "test"});
+      var nouns = getPart(allText, NOUN);
+      var verbs = getPart(allText, VERB);
+
+      sendResponse({value: verbs});
+
     }
 
   });
+
+  function getPart(obj, type){
+    var allTags = [];
+    var count = 0;
+
+    for (var tag in obj){
+      var part = [];
+
+      for(var i=0; i<obj[tag].length; i++){
+        var text = nlp(obj[tag][i]);
+        if (type === NOUN)
+          part.push(text.nouns().out('array'));
+        else if (type === VERB)
+          part.push(text.verbs().out('array'));
+      }
+
+      allTags[count] = part;
+      count = count + 1;
+    }
+
+    return allTags;
+  }
